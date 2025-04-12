f54047b982ff206c6016d6ff1d5f063273140232
keetjow <wojciechgandziarowski@gmail.com>
2025-04-13 00:04:46 +0200

    Działa servo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <pigpio.h>
 #include "enc.hpp"
+#include <unistd.h>
+#include <chrono>
+#include <cmath>
+#include <thread>
 
 /*
     BUILD
@@ -14,17 +18,34 @@
 void printVal(int val_)
 {
     float val = 360.0f*(val_/1600.0f);
-    std::cout<<val<<std::endl;
 }
+
+float f = 10.0f;
+float A = 22.5f;
 
 int main()
 {
     if(gpioInitialise() < 0) return 1;
+    gpioSetMode(22, PI_OUTPUT);
     Enc myEnc = Enc(17, 27, printVal);
 
-    while(true)
-    {
+    gpioServo(22, 1500);
+    sleep(1);
 
+    auto start = std::chrono::steady_clock::now();
+
+    while(true)
+    {        
+        auto now = std::chrono::steady_clock::now();
+        std::chrono::duration<double> elapsed = now - start;
+        double t = elapsed.count();
+
+        double angle = A*sin(2*M_PI*f*t);
+        int PWM = 1500 + (angle*(500/45));
+        gpioServo(22, PWM);
+        std::cout<<angle<<std::endl;
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     gpioTerminate();
