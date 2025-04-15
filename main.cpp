41cc8b82d7f1e630b1ae7a6c91bc7e5f033253af
keetjow <wojciechgandziarowski@gmail.com>
2025-04-15 21:04:00 +0200

    Zmiana częstotliwości

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main()
         gpioServo(22, PWM);
         std::cout<<angle<<std::endl;
         
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
     gpioTerminate();
